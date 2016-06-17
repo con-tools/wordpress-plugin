c929466b018af9e9608d6a3aaa10e3cf85aa1b8a
Oded Arbel <oded@geek.co.il>
2016-06-17 19:20:33 +0300

    split settings page into two visual sections

--- con-troll/options.php
+++ con-troll/options.php
@@ -24,10 +24,10 @@ class ConTrollSettingsPage
     {
         // This page will be under "Settings"
         add_options_page(
-            'ConTroll Settings', 
-            'ConTroll', 
-            'manage_options', 
-            'controll-admin', 
+            'ConTroll Settings',
+            'ConTroll',
+            'manage_options',
+            'controll-admin',
             array( $this, 'create_admin_page' )
         );
     }
@@ -43,13 +43,13 @@ class ConTrollSettingsPage
         $this->settings = get_option('controll-plugin-settings');
         ?>
         <div class="wrap">
-            <h2>Controll Settings</h2>           
+            <h2>Controll Settings</h2>
             <form method="post" action="options.php">
             <?php
                 // This prints out all hidden setting fields
-                settings_fields('controll_option_group');   
+                settings_fields('controll_option_group');
                 do_settings_sections('controll-settings');
-                submit_button(); 
+                submit_button();
             ?>
             </form>
         </div>
@@ -60,7 +60,7 @@ class ConTrollSettingsPage
      * Register and add settings
      */
     public function page_init()
-    {   
+    {
         register_setting(
             'controll_option_group', // Option group
             'controll-api-key' // Option name
@@ -77,54 +77,71 @@ class ConTrollSettingsPage
         );
 
         add_settings_section(
-            'controll_settings_section', // ID
-            'Controll Settings Section', // Title
-            array( $this, 'print_section_info' ), // Callback
+            'controll_settings_section_api', // ID
+            'ConTroll API Settings', // Title
+            array( $this, 'print_section_info_api' ), // Callback
             'controll-settings' // Page
-        );  
+        );
 
         add_settings_field(
             'api-key', // ID
-            'ConTroll API Key', // Title 
+            'ConTroll API Key', // Title
             array( $this, 'api_key_callback' ), // Callback
             'controll-settings', // Page
-            'controll_settings_section' // Section           
+            'controll_settings_section_api' // Section
         );
 
         add_settings_field(
-            'api-secret', 
-            'ConTroll API Secret', 
-            array( $this, 'api_secret_callback' ), 
-            'controll-settings', 
-            'controll_settings_section'
+            'api-secret',
+            'ConTroll API Secret',
+            array( $this, 'api_secret_callback' ),
+            'controll-settings',
+            'controll_settings_section_api'
+        );
+	
+        add_settings_section(
+        		'controll_settings_section_local', // ID
+        		'Site Page Configuration', // Title
+        		array( $this, 'print_section_info_local' ), // Callback
+        		'controll-settings' // Page
+        		);
+        
+        add_settings_field(
+            'my_page_url',
+            "Local URL for user's page",
+            array( $this, 'my_page_url_callback' ),
+            'controll-settings',
+            'controll_settings_section_local'
         );
 
         add_settings_field(
-            'my_page_url', 
-            "Local URL for user's page", 
-            array( $this, 'my_page_url_callback' ), 
-            'controll-settings', 
-            'controll_settings_section'
+            'event_page_url',
+            "Local URL for event page",
+            array( $this, 'event_page_url_callback' ),
+            'controll-settings',
+            'controll_settings_section_local'
         );
-
-        add_settings_field(
-            'event_page_url', 
-            "Local URL for event page", 
-            array( $this, 'event_page_url_callback' ), 
-            'controll-settings', 
-            'controll_settings_section'
-        );      
     }
 
-    /** 
+    /**
      * Print the Section text
      */
-    public function print_section_info()
+    public function print_section_info_api()
     {
         print 'Enter the API key and secret values you received when creating the convention:';
     }
 
-    /** 
+    /**
+     * Print the Section text
+     */
+    public function print_section_info_local()
+    {
+        print 'To integrate ConTroll into the Wordpress site, you should create a few pages using the ';
+        print 'ConTroll templates, and then set the fields below to the URLs (slugs) where these new pages ';
+        print 'can be found.';
+    }
+
+	/**
      * Get the settings option array and print one of its values
      */
     public function api_key_callback()
@@ -135,7 +152,7 @@ class ConTrollSettingsPage
         );
     }
 
-    /** 
+    /**
      * Get the settings option array and print one of its values
      */
     public function api_secret_callback()
@@ -146,7 +163,7 @@ class ConTrollSettingsPage
         );
     }
 
-    /** 
+    /**
      * Get the settings option array and print one of its values
      */
     public function my_page_url_callback()
@@ -157,7 +174,7 @@ class ConTrollSettingsPage
         );
     }
 
-    /** 
+    /**
      * Get the settings option array and print one of its values
      */
     public function event_page_url_callback()
