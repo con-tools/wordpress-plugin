e1d57ee4787979319955d65c2f414a89799fe7f9
Oded Arbel <oded@geek.co.il>
2017-03-07 14:48:59 +0200

    full-om uses full output buffering, to allow controll to do redirects and stuff

--- con-troll/pages/page-full-om.php
+++ con-troll/pages/page-full-om.php
@@ -8,7 +8,14 @@
  * @package ConTroll
  */
 
-$timeslots = array_map('helper_timeslot_fields', controll_api()->timeslots()->publicCatalog($filters));
+wp_enqueue_style( 'controll-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', [], '4.6.3' );
+
+if (in_array('controll_need_auth', get_post_custom_keys(get_the_ID())))
+	controll_verify_auth([]);
+
+$timeslots = controll_api()->timeslots()->publicCatalog($filters);
+
+$timeslots = array_map('helper_timeslot_fields', $timeslots);
 usort($timeslots, function($a,$b){
 	$diff = helper_controll_datetime_diff($a->start, $b->start);
 	if ($diff == 0)
@@ -18,6 +25,12 @@ usort($timeslots, function($a,$b){
 
 $locations = controll_api()->locations()->catalog();
 
+$usesPasses = controll_api()->usesPasses();
+if ($usesPasses) {
+	$passes = controll_api()->passes()->catalog();
+}
+
+ob_start();
 get_header();
 
 ?>
@@ -25,15 +38,19 @@ get_header();
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		
+		<?php if ($errorMessage): ?>
+		<h3>שגיאה: <?php echo $errorMessage ?></h3>
+		<?php endif; ?>
+
 		<?php
 		
 		the_post();
 		$com = (object)([
 				"timeslots" => $timeslots,
 				"locations" => $locations,
+				"passes" => $passes,
 		]);
 		controll_set_current_object($com);
-		ob_start();
 		the_content();
 		echo controll_parse_template($com, ob_get_clean());
 		controll_set_current_object(null);
